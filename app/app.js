5f7867b5288ff57730992024a0255d1d636191fc
Alan Plotko <alan.plotko@gmail.com>
2017-05-21 19:30:16 -0400

    Fix url paths and add compile flag for generating static site

--- app/app.js
+++ app/app.js
@@ -27,6 +27,7 @@ app.use(function(req, res, next) {
 // App locals
 app.locals.title = 'Upsilon Pi Epsilon';
 app.locals.moment = moment;
+app.locals.path = path;
 app.locals.properCase = (str) => str.charAt(0).toUpperCase() + str.substring(1);
 
 // Define navigation
@@ -78,7 +79,110 @@ app.use(function(req, res) {
     });
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, function() {
-    console.log(`Running on localhost:${port}`);
-});
+if (process.env.COMPILE) {
+    const async = require('async');
+    const fs = require('fs-extra');
+    const pug = require('pug');
+
+    for (let i = 0; i < app.locals.navigation.length; i++) {
+        if (app.locals.navigation[i].url === '/') {
+            app.locals.navigation[i].url = 'index.html';
+        } else {
+            app.locals.navigation[i].url += '.html';
+            app.locals.navigation[i].url = app.locals.navigation[i].url
+                .substr(1);
+        }
+    }
+
+    let tasks = [];
+
+    let createDir = function(dir) {
+        return function(callback) {
+            fs.ensureDir(dir, callback);
+        };
+    };
+
+    let writeFile = function(src, dst) {
+        return function(callback) {
+            let html = pug.renderFile(src, app.locals);
+            html = html.replace(/href="\/(\w+)"/, 'href="$1.html"');
+            fs.writeFile(dst, html, function(err) {
+                if(err) {
+                    console.log(err);
+                    return callback(err);
+                }
+
+                console.log('✔ Saved to ' + dst);
+                return callback(null, true);
+            });
+        }
+    };
+
+    let copy = function(src, dst) {
+        return function(callback) {
+            fs.copy(src, dst, function(err) {
+                if (err) return callback(err);
+                console.log('✔ Copied ' + src + ' to ' + dst);
+                callback(null, true);
+            });
+        };
+    };
+
+    let getPhotos = function(folder) {
+        return function(callback) {
+            fs.readdir('static/assets/img/photos/' + folder,
+                function(error, data) {
+                    if (error) return callback(error);
+
+                    let photos = [];
+                    for (var i = 0; i < data.length; i++) {
+                        if (path.extname(data[i]) === '.jpg') {
+                            photos.push(data[i]);
+                        }
+                    }
+                    app.locals[folder] = photos;
+                    return callback(null, photos);
+            });
+        }
+    };
+
+    tasks.push(createDir('static'));
+    tasks.push(createDir('static/jquery'));
+    tasks.push(createDir('static/moment'));
+    tasks.push(createDir('static/semantic'));
+
+    tasks.push(copy('assets', 'static/assets'));
+    tasks.push(copy('node_modules/jquery/dist/jquery.min.js',
+        'static/jquery/jquery.min.js'));
+    tasks.push(copy('node_modules/moment/min/moment.min.js',
+        'static/moment/moment.min.js'));
+    tasks.push(copy('node_modules/semantic-ui-css/semantic.min.css',
+        'static/semantic/semantic.min.css'));
+    tasks.push(copy('node_modules/semantic-ui-css/semantic.min.js',
+        'static/semantic/semantic.min.js'));
+
+    tasks.push(getPhotos('spring17'));
+    tasks.push(getPhotos('fall16'));
+    tasks.push(writeFile('views/photos.pug', 'static/photos.html'));
+
+    tasks.push(writeFile('views/index.pug', 'static/index.html'));
+    tasks.push(writeFile('views/about.pug', 'static/about.html'));
+    tasks.push(writeFile('views/news.pug', 'static/news.html'));
+    tasks.push(writeFile('views/tutoring.pug', 'static/tutoring.html'));
+    tasks.push(writeFile('views/contact.pug', 'static/contact.html'));
+
+    async.series(tasks, function(err, results) {
+        if (err) {
+            console.log('\nError generating static site');
+            process.exit(1);
+        }
+
+        console.log('\nGenerated static site successfully');
+        process.exit(0);
+    });
+} else {
+    const port = process.env.PORT || 3000;
+    app.listen(port, function() {
+        console.log(`Running on localhost:${port}`);
+    });
+}
